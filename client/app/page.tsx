969f24252d13e06c918b9f4d95f48b52b3ce554f
Matias Luduena <matiasluduena23@gmail.com>
2025-02-14 13:01:25 -0300

    nextjs & shadcn setup

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<main className="flex flex-col gap-8 row-start-2 items-center mt-20">
+			<h1 className="text-3xl">No Country Proyecto</h1>
+			<Button>Iniciar Sesion</Button>
+		</main>
+	);
+}
