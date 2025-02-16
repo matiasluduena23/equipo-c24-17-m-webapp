64014a67450a0bafd159f4bcf91e83b2538ea058
Matias Luduena <matiasluduena23@gmail.com>
2025-02-16 18:20:17 -0300

    change server docker file path to build

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef12c3896780101aaad93b2a44880c73a07b6719")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab511541da8f58131c30f3e21cfa74aa48ccc8a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
